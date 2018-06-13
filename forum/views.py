e811ae5bbaf9d3bd8daf6bd7adb026007809ddc6
SoyujBasnet <bsoyuj@gmail.com>
2018-06-13 23:25:58 +0545

    adding more stuff ,css for forum ,handling the login_required()thingy

--- forum/views.py
+++ forum/views.py
@@ -3,7 +3,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class ForumIndexView(TemplateView):
     def get(self, request, *args, **kwargs):
+        context=super(ForumIndexView,self).get_context_data(*args,**kwargs)
+        context={'forum':True}
         if not request.user.is_authenticated:
-            return render(request,'forum/forum_not_logged.html',{})
+            return render(request,'forum/forum_not_logged.html',context)
         else:
-            return render(request, 'forum/forum.html', {})
+            return render(request, 'forum/forum.html', context)
