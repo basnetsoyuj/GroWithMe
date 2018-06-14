61ea06f252431ddc5b8d8e9edfa7cfc37cc74eb1
SoyujBasnet <bsoyuj@gmail.com>
2018-06-14 08:45:44 +0545

    separate cssfor forum and rick picture

--- forum/views.py
+++ forum/views.py
@@ -6,6 +6,6 @@ class ForumIndexView(TemplateView):
         context=super(ForumIndexView,self).get_context_data(*args,**kwargs)
         context={'forum':True}
         if not request.user.is_authenticated:
-            return render(request,'forum/forum_not_logged.html',context)
+            return render(request'forum/forum_not_logged.html',context)
         else:
-            return render(request, 'forum/forum.html', context)
+            return render(request,'forum/forum.html', context)
--- growithme/settings.py
+++ growithme/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'home','accounts','forum',
+    'forum','home','accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
