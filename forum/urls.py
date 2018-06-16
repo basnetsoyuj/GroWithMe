d4585e3612ba8b2ace1a652d504c8068cba26d0f
SoyujBasnet <bsoyuj@gmail.com>
2018-06-16 23:31:20 +0545

    explore page started,bug fixes ,and new search button on the navbar

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ForumIndexView,ForumSearchView
+from .views import ForumIndexView,ForumSearchView,ForumExploreView
 app_name="forum"
 urlpatterns = [
     path('',ForumIndexView.as_view(),name="index"),
-    path('explore/',ForumIndexView.as_view(),name="explore"),
+    path('explore/',ForumExploreView.as_view(),name="explore"),
     path('search',ForumSearchView.as_view(),name="search"),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
 # Create your views here.
 class ForumIndexView(TemplateView):
     def get(self, request, *args, **kwargs):
@@ -20,3 +20,13 @@ class ForumSearchView(TemplateView):
             context['logged_in']=True
             return render(request,'forum/forum_search.html', context)
 
+class ForumExploreView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        context=super(ForumExploreView,self).get_context_data(*args,**kwargs)
+        context={'forum':True}
+        if not request.user.is_authenticated:
+            return render(request,'forum/forum_not_logged.html',context)
+        else:
+            context['logged_in']=True
+            context['explore']=True
+            return render(request,'forum/forum_explore.html', context)
