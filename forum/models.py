410e90e2e6f6953cad0e47f3fa47163beaa076c0
SoyujBasnet <bsoyuj@gmail.com>
2018-06-18 23:45:11 +0545

    Added more columns to database , redesigned the views page

--- forum/models.py
+++ forum/models.py
@@ -2,11 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 class ForumSection(models.Model):
     name=models.CharField(max_length=75)
-    admin=models.ForeignKey(User,on_delete=models.CASCADE,null=False)
-    picture_link=models.CharField(max_length=250)
+    admin=models.ForeignKey(User,on_delete=models.CASCADE,default=1)
+    picture_link=models.CharField(max_length=250,default="/forum/images/default_section.png")
     section_link=models.CharField(max_length=250)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     name=models.CharField(max_length=250)
+    author=models.ForeignKey(User,on_delete=models.CASCADE,default=1)
     section=models.ForeignKey(ForumSection,on_delete=models.CASCADE)
     content=models.TextField()
+    def __str(self):
+        return self.name
--- forum/views.py
+++ forum/views.py
@@ -1,32 +1,61 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.generic import TemplateView,ListView
+from .models import ForumSection
 # Create your views here.
 class ForumIndexView(TemplateView):
-    def get(self, request, *args, **kwargs):
-        context=super(ForumIndexView,self).get_context_data(*args,**kwargs)
-        context={'forum':True}
-        if not request.user.is_authenticated:
-            return render(request,'forum/forum_not_logged.html',context)
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['forum'] = True
+        if self.request.user.is_authenticated:
+            context['logged_in'] = True
+            return context
         else:
-            context['logged_in']=True
-            return render(request,'forum/forum.html', context)
-class ForumSearchView(TemplateView):
-    def get(self, request, *args, **kwargs):
-        context=super(ForumSearchView,self).get_context_data(*args,**kwargs)
-        context={'forum':True}
-        if not request.user.is_authenticated:
-            return render(request,'forum/forum_not_logged.html',context)
-        else:
-            context['logged_in']=True
-            return render(request,'forum/forum_search.html', context)
+            return context
 
-class ForumExploreView(TemplateView):
-    def get(self, request, *args, **kwargs):
-        context=super(ForumExploreView,self).get_context_data(*args,**kwargs)
-        context={'forum':True}
-        if not request.user.is_authenticated:
-            return render(request,'forum/forum_not_logged.html',context)
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            template_name = "forum/forum.html"
         else:
-            context['logged_in']=True
-            context['explore']=True
-            return render(request,'forum/forum_explore.html', context)
+            template_name = "forum/forum_not_logged.html"
+        return template_name
+
+class ForumSearchView(TemplateView):
+    def get_context_data(self, *args,**kwargs):
+        context = super().get_context_data(*args,**kwargs)
+        context['forum'] = True
+        if self.request.user.is_authenticated:
+            context['logged_in'] = True
+            return context
+        else:
+            return context
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            template_name = "forum/forum_search.html"
+        else:
+            template_name = "forum/forum_not_logged.html"
+        return template_name
+
+class ForumExploreView(ListView):
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            object_list = ForumSection.objects.all()
+            return object_list
+        else:
+            return None
+
+    def get_context_data(self, *args,**kwargs):
+        context = super().get_context_data(*args,**kwargs)
+        context['forum'] = True
+        if self.request.user.is_authenticated:
+            context['logged_in'] = True
+            context['explore'] = True
+            return context
+        else:
+            return context
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            template_name = "forum/forum_explore.html"
+        else:
+            template_name = "forum/forum_not_logged.html"
+        return template_name
