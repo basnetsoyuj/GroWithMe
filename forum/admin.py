eace77466231de19bc61ae379dba667d2798820f
SoyujBasnet <bsoyuj@gmail.com>
2018-06-18 07:37:45 +0545

    working on the backend of forum

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import ForumSection,Post
 # Register your models here.
+admin.site.register(ForumSection)
+admin.site.register(Post)
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+class ForumSection(models.Model):
+    name=models.CharField(max_length=75)
+    admin=models.ForeignKey(User,on_delete=models.CASCADE,null=False)
+    picture_link=models.CharField(max_length=250)
+    section_link=models.CharField(max_length=250)
 
-# Create your models here.
+class Post(models.Model):
+    name=models.CharField(max_length=250)
+    section=models.ForeignKey(ForumSection,on_delete=models.CASCADE)
+    content=models.TextField()
