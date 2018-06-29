4c1ffd695c270e917fd3666554438b74d26b4f34
SoyujBasnet <bsoyuj@gmail.com>
2018-06-29 20:46:02 +0545

    Login automatic redirect with ?next=... feature

--- forum/views.py
+++ forum/views.py
@@ -1,84 +1,47 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView,ListView,DetailView,CreateView
 from .models import ForumSection
 from django.shortcuts import get_object_or_404
 from .forms import PostCreationForm
-class ForumIndexView(TemplateView):
+class ForumIndexView(LoginRequiredMixin,TemplateView):
+    login_url = "/accounts/login/"
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['forum'] = True
-        if self.request.user.is_authenticated:
-            context['logged_in'] = True
+        context.update({'forum': True, 'logged_in': True})
         return context
+    template_name = "forum/forum.html"
 
-    def get_template_names(self):
-        if self.request.user.is_authenticated:
-            template_name = "forum/forum.html"
-        else:
-            template_name = "forum/forum_not_logged.html"
-        return template_name
-
-class ForumSearchView(TemplateView):
+class ForumSearchView(LoginRequiredMixin,TemplateView):
     def get_context_data(self, *args,**kwargs):
         context = super().get_context_data(*args,**kwargs)
-        context['forum'] = True
-        if self.request.user.is_authenticated:
-            context['logged_in'] = True
+        context.update({'forum': True, 'logged_in': True})
         return context
-    def get_template_names(self):
-        if self.request.user.is_authenticated:
-            template_name = "forum/forum_search.html"
-        else:
-            template_name = "forum/forum_not_logged.html"
-        return template_name
+    template_name = "forum/forum_search.html"
 
-class ForumExploreView(ListView):
+class ForumExploreView(LoginRequiredMixin,ListView):
     def get_queryset(self):
-        if self.request.user.is_authenticated:
-            object_list = ForumSection.objects.all()
-            return object_list
-        else:
-            return None
+        object_list = ForumSection.objects.all()
+        return object_list
 
     def get_context_data(self, *args,**kwargs):
         context = super().get_context_data(*args,**kwargs)
-        context['forum'] = True
-        if self.request.user.is_authenticated:
-            context['logged_in'] = True
-            context['explore'] = True
+        context.update({'forum':True,'logged_in':True,'explore':True})
         return context
-    def get_template_names(self):
-        if self.request.user.is_authenticated:
-            template_name = "forum/forum_explore.html"
-        else:
-            template_name = "forum/forum_not_logged.html"
-        return template_name
+    template_name = "forum/forum_explore.html"
 
 
-class ForumSectionDetailView(DetailView):
+class ForumSectionDetailView(LoginRequiredMixin,DetailView):
     def get_object(self):
         section_link=self.kwargs.get("section_link")
         obj=get_object_or_404(ForumSection,section_link=section_link)
         return obj
     def get_context_data(self, *args,**kwargs):
         context = super().get_context_data(*args,**kwargs)
-        context['forum'] = True
-        if self.request.user.is_authenticated:
-            context['logged_in'] = True
-            context['explore'] = True
+        context.update({'forum': True, 'logged_in': True, 'explore': True})
         return context
-    def get_template_names(self):
-        if self.request.user.is_authenticated:
-            template_name = "forum/forum_detail.html"
-        else:
-            template_name = "forum/forum_not_logged.html"
-        return template_name
+    template_name = "forum/forum_detail.html"
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     form_class = PostCreationForm
-    def get_template_names(self):
-        if self.request.user.is_authenticated:
-            template_name = "forum/post_create.html"
-        else:
-            template_name = "forum/forum_not_logged.html"
-        return template_name
+    template_name = "forum/post_create.html"
     success_url = "/forum/explore/"
