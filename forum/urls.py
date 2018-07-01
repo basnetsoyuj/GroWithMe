7d4c064ad12de1ab7ce83dd3d2a7f3bec9992c20
SoyujBasnet <bsoyuj@gmail.com>
2018-07-01 20:15:44 +0545

    PostCreationForm and Subscribe Working Button

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,re_path
-from .views import ForumIndexView,ForumSearchView,ForumExploreView,ForumSectionDetailView,PostCreateView
+from .views import ForumIndexView,ForumSearchView,ForumExploreView,ForumSectionDetailView,PostCreateView,Subscribe
+
 app_name="forum"
 urlpatterns = [
     path('',ForumIndexView.as_view(),name="index"),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('search/',ForumSearchView.as_view(),name="search"),
     re_path(r'^sections/(?P<section_link>[\w-]+)/$',ForumSectionDetailView.as_view(),name="sections"),
     path('create/', PostCreateView.as_view(), name="create"),
+    path('subscribe/<int:pk>', Subscribe.as_view(), name="subscribe"),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView,ListView,DetailView,CreateView
+from django.views.generic import TemplateView,ListView,DetailView,CreateView,View
 from .models import ForumSection
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404,reverse,redirect
 from .forms import PostCreationForm
+from django.contrib.auth.models import User
 class ForumIndexView(LoginRequiredMixin,TemplateView):
     login_url = "/accounts/login/"
     def get_context_data(self, *args, **kwargs):
@@ -36,12 +37,31 @@ class ForumSectionDetailView(LoginRequiredMixin,DetailView):
         obj=get_object_or_404(ForumSection,section_link=section_link)
         return obj
     def get_context_data(self, *args,**kwargs):
+        section_link = self.kwargs.get("section_link")
+        obj = get_object_or_404(ForumSection, section_link=section_link)
+        subscribers = list(obj.subscribers.all())
+        is_subscribed= self.request.user in subscribers
         context = super().get_context_data(*args,**kwargs)
-        context.update({'forum': True, 'logged_in': True, 'explore': True})
+        context.update({'forum': True, 'logged_in': True, 'explore': True ,'is_subscribed':is_subscribed})
         return context
     template_name = "forum/forum_detail.html"
 
 class PostCreateView(LoginRequiredMixin,CreateView):
+    def get_context_data(self, *args,**kwargs):
+        context = super().get_context_data(*args,**kwargs)
+        context.update({'forum':True,'logged_in':True,'explore':True})
+        return context
     form_class = PostCreationForm
     template_name = "forum/post_create.html"
     success_url = "/forum/explore/"
+
+class Subscribe(LoginRequiredMixin,View):
+    def get(self,request,*args,**kwargs):
+        section_pk=kwargs['pk']
+        loggedin_user=self.request.user
+        section = get_object_or_404(ForumSection, id=section_pk)
+        subscribers = list(section.subscribers.all())
+        is_subscribed = self.request.user in subscribers
+        if section and not is_subscribed :
+            section.subscribers.add(loggedin_user)
+        return redirect('forum:sections',section_link=section.section_link)
